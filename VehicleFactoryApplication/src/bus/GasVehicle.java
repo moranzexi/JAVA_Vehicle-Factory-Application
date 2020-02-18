8a8f6f42bb78af63f15dd7fe77a3d653596847fb
Aurora <32115999+moranzexi@users.noreply.github.com>
2020-02-18 09:59:33 -0500

    Update GasVehicle.java

--- VehicleFactoryApplication/src/bus/GasVehicle.java
+++ VehicleFactoryApplication/src/bus/GasVehicle.java
@@ -5,30 +5,7 @@ import java.util.Scanner;
 
 public class GasVehicle extends Vehicle{
 	private float gasoline;
-	public float calculateGasoline(float price,long seconds)
-	{
-		
-		Random read=new Random();
-		if(price>=200000)
-		{
-			
-			float number=(float) (read.nextFloat()*1.79+2.2);		//2.20-3.99		
-			System.out.println("the gasoline per second for your car of this run is "+number+" gas");	
-			 return number*seconds;
-		}
-		if(price>=100000&&price<200000)
-		{
-			float number=(float) (read.nextFloat()*1.99+4);		//4.00-5.99	
-			System.out.println("the gasoline per second for your car of this run is "+number+" gas");
-			 return number*seconds;
-		}
-		else
-		{
-			float number=(float) (read.nextFloat()*2.99+6);		//6.00-8.99		
-			System.out.println("the gasoline per second for your car of this run is "+number+" gas");
-			 return number*seconds;
-		}
-	}
+	
 	public void setGasolin(float value)
 	{
 		this.gasoline=value;
@@ -46,19 +23,7 @@ public class GasVehicle extends Vehicle{
 		super(id,make,model,count,date,type,p,time);
 		this.gasoline=gas;
 	}
-	public void makeTrip()
-	{
-		this.counter=100;
-		this.gasoline=7.77f;//f means float, limit to 6 digits
-		long time = System.currentTimeMillis();
-		Scanner scan = new Scanner(System.in);
-		//Random read=new Random();
-		System.out.println("press enter to start to run the car");		
-		scan.nextLine();
-		 long nowMillis = System.currentTimeMillis();
-		 long seconds=(nowMillis - time) / 1000;
-		 setSecond(seconds);
-	}
+	
 	public float getMilesPerGallon()
 	{
 		return this.counter/this.gasoline;
